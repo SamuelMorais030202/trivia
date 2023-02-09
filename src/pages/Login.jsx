dfe76f3d7c3ba431ab3049e0cc799a259c9f150e
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-02-09 16:02:36 -0300

    Iniciando o Projeto

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Página de login</h1>
+      </div>
+    );
+  }
+}
+
+export default Login;
